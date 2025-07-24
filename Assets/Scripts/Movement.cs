eba004c17ea587d9239b4dc20307a2e490e2571f
Zachary Culpepper <zculp4579@gmail.com>
2025-07-24 07:56:51 -0500

    First Jump Commit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,16 +4,21 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float jumpStrength = 100f;
     [SerializeField] InputAction right;
     [SerializeField] InputAction left;
+    [SerializeField] InputAction jump;
 
     Rigidbody rb;
+    bool canJump = true;
+    bool canMove = true;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         right.Enable();
         left.Enable();
+        jump.Enable();
 
         rb = GetComponent<Rigidbody>();
     }
@@ -22,17 +27,29 @@ public class Movement : MonoBehaviour
     void Update()
     {
         Move();
+        Jump();
     }
 
     void Move()
     {
-        if (right.IsPressed())
+        if (right.IsPressed() && canMove)
         {
-            rb.AddRelativeForce(Vector3.right * moveSpeed);
+            rb.AddRelativeForce(Vector3.right * moveSpeed * Time.fixedDeltaTime);
         }
-        if (left.IsPressed())
+        if (left.IsPressed() && canMove)
         {
-            rb.AddRelativeForce(Vector3.left * moveSpeed);
+            rb.AddRelativeForce(Vector3.left * moveSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    void Jump()
+    {
+        if (jump.IsPressed() && canJump)
+        {
+            Debug.Log("JUMP");
+            canMove = false;
+            canJump = false;
+            rb.AddRelativeForce(Vector3.up * jumpStrength);
         }
     }
 }
